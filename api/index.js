36f6e9aaeef66cd31b07f5116f3936dd90ab5d04
Indra Saepudin <indrasaepudin212@gmail.com>
2025-11-25 23:54:34 +0700

    dahh2

--- api/index.js
+++ api/index.js
@@ -1,2 +1,21 @@
-import app from "../src/index.js";
-export default app;
+import express from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+import { prisma } from "../src/config/db.js";
+import { apiRouter } from "../src/routes/index.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Serverless backend running on Vercel",
+  });
+});
+
+app.use("/api", apiRouter);
+
+export default serverless(app);
