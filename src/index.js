d901f2b407be11d24182e879a7798d84837274e4
Indra Saepudin <indrasaepudin212@gmail.com>
2025-11-30 02:10:47 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import { apiRouter } from "./routes/index.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
